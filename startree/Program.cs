acd4f3f1d71795bde7761c669c60cb4bf98d7f49
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-12 12:41:16 +0900

    문제6번 startree

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,19 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            //라인 총 8줄에 마지막 별 개수 15
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 8 - i; j > 1; j--)
+                {
+                    Console.Write(" ");//라인 신경 쓴거임
+                }
+                for(int j = 0; j <= (2 * i); j++)//별찍는거임
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
